9956fedb02a6f799677dab73302cf873a7bdffd3
bagaswidodo <bagaswidodo@gmail.com>
2015-11-29 10:45:52 +0700

    add migration lokasi table

---
+++ database/migrations/2015_11_28_225145_create_lokasi_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLokasiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lokasi', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->float('latitude');
+            $table->float('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('lokasi');
+    }
+}

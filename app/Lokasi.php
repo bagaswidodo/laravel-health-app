9508f518945b9fa4b25715d65c60764a128b49fc
bagaswidodo <bagaswidodo@gmail.com>
2015-11-29 10:45:09 +0700

    Added Lokasi model

---
+++ app/Lokasi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lokasi extends Model
+{
+    protected $table = 'lokasi';
+//    protected $fillable = ['deskripsi'];
+//    protected $primaryKey = 'tipe_id';
+}

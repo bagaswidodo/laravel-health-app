50f6187d6b764e64d872c40d86744a54a99faf0e
bagaswidodo <bagaswidodo@gmail.com>
2016-02-05 21:09:50 +0700

    cleanup frontend

--- resources/views/material/detail.blade.php
+++ resources/views/material/detail.blade.php
@@ -1,19 +1,16 @@
 @extends('material.layout.paralax')
 
 @section('content')
-
   <!-- container -->
   <div class="container">
     <div class="section">
-
       <!--   Icon Section   -->
       <div class="row">
         <div class="col s12 m12">
          <h1>{{ $f->nama_faskes }}</h1>
          <h5>{{ $f->alamat }}</h5>
          <hr>
- 
-          <!-- tabs -->
+           <!-- tabs -->
           <div class="row">
               <div class="col s12">
                 <ul class="tabs teal">
@@ -22,7 +19,6 @@
                   <li class="tab col s4"><a href="#test2" class="teal-text text-lighten-5">Dokter</a></li>
                   @endif
                   <li class="tab col s4"><a href="#test3" class="teal-text text-lighten-5">Rute </a></li>
-                  <!-- <li class="tab col s4"><a href="#" class="teal-text text-lighten-5">Foo</a></li> -->
                 </ul>
               </div>
                   <div id="test1" class="col s12">
@@ -35,7 +31,6 @@
                   	4=> "jumat",
                   	5=> "sabtu",
                   	6=> "minggu"];
-
                   	?>
                   		<thead>
                   			<tr>
@@ -60,11 +55,8 @@
 	                  		</tr>
 	                  		@endforeach
                   		</tbody>
-                  	</table>
-                  	
-
+                  	</table>                	
                   </div>
-
 				@if($f->tipe_id == 1 || $f->tipe_id == 2)
                   <div id="test2" class="col s12">
                   	<ul class="collection">
@@ -75,50 +67,34 @@
 				      <button class="btn waves-effect waves-light" onclick="ambilJadwal({{ $d->dokter_id }})">Lihat Jadwal</button>
 				    </li>
 				    @endforeach
-
                   </div>
                   @endif
                    <div id="test3" class="col s12">
                    	<p>
                    		<?php 
                    		$latlng = explode(",",$location);
-
                    		?>
 			    		<a class="btn btn-primary btn-lg" onclick="rute({{$latlng[0]}},{{$latlng[1]}})">
 			    		Tampilkan Rute</a>
 		    		</p>
-
 					<div class="row">
-						<div class="col-md-6">
-							<div class="panel panel-default">
-							  <div class="panel-heading">Lokasi Di Peta</div>
-							  <div class="panel-body">
-							     <div id="map" style="height:200px"></div>
-							  </div>
-							</div>
+						<div class="col s6 m6">
+							<!-- routing map -->
+							<div id="map" style="height:200px"></div>
 						</div>
-
-						<div class="col-md-6">
-							<!-- <button onclick="rute()">Tampilkan Rute Ke tujuan</button> -->
-							<div class="panel panel-default">
-							  <div class="panel-heading">Rute</div>
-							  <div class="panel-body">
-							    	<div id="panel"></div>
-							  </div>
-							</div>
+						<div class="col s6 m6">
+							<!-- node to node -->
+							<div id="panel"></div>
 						</div>
 					</div>
-                   </div>
-              
-              
-             
+                   </div>           
             </div>
           <!-- end tabs -->
         </div>
     </div>
   </div>
   </div>
-
+  <!-- modal jadwal praktek dokter -->
   <div id="modal1" class="modal">
     <div class="modal-content">
       <h4 id="nama_dokter">Modal Header</h4>
@@ -129,7 +105,6 @@
       	</tr>
       </table>
       <p id="id_dokter"></p>
-
       <!-- preloader -->
 			<div class="preloader-wrapper big active" style="display:none" id="preloader">
 		      <div class="spinner-layer spinner-blue">
@@ -148,126 +123,61 @@
       <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Tutup</a>
     </div>
   </div>
+  <!-- end modal -->
 @stop
 
 @section('coding')
 <script>
- $(document).ready(function(){
-    $('ul.tabs').tabs('select_tab', 'tab_id');
-  });
-</script>
-<script>
-	function ambilJadwal(dokterID)
-	{
-		$('#modal1').openModal();
-		var restUrl = '{{ url() }}/api/dokter/praktek/' + dokterID;
-		$.ajax({
-			type:'GET',
-		    url: restUrl,
-		    beforeSend: function() {
-			     $('#preloader').show();
-			},
-			complete: function(){
-			     $('#preloader').hide();
-			},
-		  	success:function(jadwal)
-	        {
-	        	$('#nama_dokter').html(jadwal.nama_dokter);
-	        	// $.each(jadwal.data);
-				$('#schedule').html("");
-	        	var weekdays = ['Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu'];
-	        	$.each(jadwal.data, function(i, j){
-                      $('#schedule').append("<tr><td>"
-                      	+ weekdays[j.hari] 
-                      	+ "</td><td>" 
-                      	+ j.jam_mulai + "-" + j.jam_selesai
-                      	+"</td></tr>");
-	             });
-	        },
-	        error:function(){
-	        	alert('Ooops, Something wrong');
-        	}
-		});
-
-		
-
-	}
+var lat={{$latlng[0]}};
+var lng ={{$latlng[1]}};
+$(document).ready(function(){
+	$('ul.tabs').tabs('select_tab', 'tab_id');
+});
+function ambilJadwal(dokterID)
+{
+	$('#modal1').openModal();
+	var restUrl = '{{ url() }}/api/dokter/praktek/' + dokterID;
+	$.ajax({
+		type:'GET',
+	    url: restUrl,
+	    beforeSend: function() {
+		     $('#preloader').show();
+		},
+		complete: function(){
+		     $('#preloader').hide();
+		},
+	  	success:function(jadwal)
+        {
+        	$('#nama_dokter').html(jadwal.nama_dokter);
+			$('#schedule').html("");
+        	var weekdays = ['Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu'];
+        	$.each(jadwal.data, function(i, j){
+                  $('#schedule').append("<tr><td>"
+                  	+ weekdays[j.hari] 
+                  	+ "</td><td>" 
+                  	+ j.jam_mulai + "-" + j.jam_selesai
+                  	+"</td></tr>");
+             });
+        },
+        error:function(){
+        	alert('Ooops, Something wrong');
+    	}
+	});
+}
 </script>
 <!-- map route -->
 <!-- gmap -->
 	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+	<script src="{{ asset('js/tools/gmap.js') }}"></script>
 	<script type="text/javascript">
-	//init map
-	google.maps.event.addDomListener(window,'load',function(){
-	  var homeLatlng = new google.maps.LatLng(-7.33,110.5); 
-	  
-	  var myOptions = {
-	    zoom: 15,
-	    center: homeLatlng,
-	    mapTypeId: google.maps.MapTypeId.ROADMAP
-	  };
-	  
-	  map = new google.maps.Map(document.getElementById("map"), myOptions);
-
-	   infowindow = new google.maps.InfoWindow({
-	        content: "loading..."
-	    });
-
-	   var marker = new google.maps.Marker({
-				        position: new google.maps.LatLng({{$latlng[0]}},{{$latlng[1]}}),
-				        map: map,
-				        //icon : 'http://maps.google.com/mapfiles/kml/shapes/man.png',
-				        title: 'loc'
-				    });
-	});
-
-	//routing
 	function rute(lat,lng)
 	{
-		//alert(lat + "," + lng);
 		awal = lat + "," + lng;
-
 		destLat = {{$f->latitude}} ;
 		destLng = {{$f->longitude}};
 		tujuan = destLat + "," + destLng;
-		//console.log(tujuan);
 		routing(awal , tujuan);
-		// routing(tujuan,awal);
-		// console.log(awal + '->' + tujuan);
 	}
-
-
-	function routing(awal,tujuan)
-	{
-	 var directionsService = new google.maps.DirectionsService();
-	     var directionsDisplay = new google.maps.DirectionsRenderer();
-	     console.log(awal + "=>" + tujuan);
-	     document.getElementById('panel').innerHTML = "";
-
-	     var map = new google.maps.Map(document.getElementById('map'), {
-	       zoom:7,
-	       mapTypeId: google.maps.MapTypeId.ROADMAP
-	     });
-
-	     directionsDisplay.setMap(map);
-	     directionsDisplay.setPanel(document.getElementById('panel'));
-
-	     var request = {
-
-	       origin: awal,
-	       destination: tujuan,
-	       travelMode: google.maps.DirectionsTravelMode.DRIVING
-	     };
-
-	     directionsService.route(request, function(response, status) {
-	       if (status == google.maps.DirectionsStatus.OK) {
-	         directionsDisplay.setDirections(response);
-
-	         var point = response.routes[ 0 ].legs[ 0 ];
-	       }
-	     });
-	}
-
 	</script>
 	<!-- end gmap -->
 @stop

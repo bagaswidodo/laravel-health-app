714c9410879c6048c8c9db2ae19926be1cdcaa27
bagaswidodo <bagaswidodo@gmail.com>
2016-02-05 20:28:53 +0700

    add getting nearby location

--- app/Http/Controllers/NearbyController.php
+++ app/Http/Controllers/NearbyController.php
@@ -108,10 +108,46 @@ class NearbyController extends Controller
 
     }
 
+    public function locationMaterial($location, $jarak = 1)
+    {
+            // dd($location);
+            $l = explode("," , $location);
+            $latitude = $l[0];
+            $longitude = $l[1];
+
+            $start = microtime(true);
+            $nearby = DB::select('select fo.hari,fo.jam_buka,fo.jam_tutup,f.faskes_id,f.nama_faskes,f.alamat,f.latitude,f.longitude,f.tipe_id,
+             6371*(2*ASIN(SQRT(POWER(SIN((abs(f.latitude) - abs('.$latitude.')) * pi()/180 / 2), 2) +
+             COS(abs('.$longitude.') * pi()/180 ) * COS(abs(f.latitude) * pi()/180)
+             * POWER(SIN((f.longitude - '.$longitude.') *pi()/180 / 2), 2) ))) as jarak from faskes f
+             join faskes_open fo on fo.faskes_id = f.faskes_id
+
+             where fo.hari = WEEKDAY(now()) AND TIME(NOW()) BETWEEN fo.jam_buka AND fo.jam_tutup
+              AND TIME(NOW()) NOT BETWEEN fo.jam_mulai_istirahat and fo.jam_selesai_istirahat
+               having jarak < ' . $jarak . ' ORDER BY jarak ASC');
+            $elapsed_time = microtime(true) - $start;
+
+            $distance = $jarak;
+            $waktu = $elapsed_time;
+            // return view('material.nearby');
+
+            return view('material.sekitar', compact('nearby','location','distance','waktu'));
+            // return view('material.nearby', compact('nearby','location','distance','waktu'));
+
+    }
+
     public function detail($latlng,$faskes_id){
             $f = \App\Faskes::findOrFail($faskes_id);
             $location = $latlng;
             return view('user.detail',compact('f', 'location'));
+    }
+
+    public function materialDetail($latlng,$faskes_id){
+            $f = \App\Faskes::findOrFail($faskes_id);
+            $works = $f->works()->get();
+
+            $location = $latlng;
+            return view('material.detail',compact('f', 'location','works'));
     }
 
     public function euclidean($latitude,$longitude,$jarak=1)
